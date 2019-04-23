77d20f18b19ae597f0c400ec9202d1a15775359a
ambevill <abev236@gmail.com>
2019-04-23 10:16:12 +0200

    Update source.R

--- R/source.R
+++ R/source.R
@@ -1,30 +1,27 @@
-library(shiny)
-library(shinyjs)
-
 ui_function = function()
 {
-  tagList(
-    useShinyjs(),
-    actionButton('only_button', 'This button should be disabled')
+  shiny::tagList(
+    shinyjs::useShinyjs(),
+    shiny::actionButton('only_button', 'This button should be disabled')
   )
 }
 
 ui_static = ui_function()
 
-server = shinyServer(function(input, output, session) {
+server = shiny::shinyServer(function(input, output, session) {
   
-  observe({ disable('only_button') })
+  shiny::observe({ shinyjs::disable('only_button') })
   
 })
 
 #' @export
 run_with_ui_function = function()
 {
-  runApp(list(ui=ui_function(), server=server))
+  shiny::runApp(list(ui=ui_function(), server=server))
 }
 
 #' @export
 run_with_ui_static = function()
 {
-  runApp(list(ui=ui_static, server=server))
+  shiny::runApp(list(ui=ui_static, server=server))
 }
