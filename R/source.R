98a472eac0b3f313b1fb109b1622e1a86ab1adf6
ambevill <abev236@gmail.com>
2019-04-23 10:26:28 +0200

    Update source.R

--- R/source.R
+++ R/source.R
@@ -20,11 +20,13 @@ server = shiny::shinyServer(function(input, output, session) {
 #' @export
 run_with_ui_function = function()
 {
+  cat("Creating UI and running app...", fill=TRUE)
   shiny::runApp(list(ui=ui_function(), server=server))
 }
 
 #' @export
 run_with_ui_static = function()
 {
+  cat("Running app using UI created within the package...", fill=TRUE)
   shiny::runApp(list(ui=ui_static, server=server))
 }
